b8faa3db48f49b75d6f326c391b95746e1a9802c
Sahad M <sahad.mohamed@gmail.com>
2015-07-20 14:47:07 +0200

    Correctif mineur au/en

--- test.php
+++ test.php
@@ -10,25 +10,27 @@
 
 function getPreposition_FR ($country) 
 {
-	$array_preposition = array ('en', 'à', 'au', 'aux');
-	$suffix_preposition = '';
+	$prep_country = '';
 
-	if  (preg_match('/(agne|iche|ran|ance|lande|èce|ogne|aine|eine|ène|rse|la|ence|erre|dor|ie|ak|ine|é|ée|rica|oupe|oire|iche|ïdjan|an|sud|ège|ède|ypte|ique|teur|isse)$/i', $country, $matches) )
-			$suffix_preposition .= $array_preposition[0];
-	else if (preg_match('/(ar|ram|eille|nade|ris|ba|na|ma|co|ago|alu|boul|bul|eïn|ain|nei|ti|ok|pre|orre|lize|bourg|ypre|grenadine|ey)$/i', $country, $matches))
-			$suffix_preposition .= $array_preposition[1];
-	else if (preg_match('/(aso|am|oc|os|ana|odge|bas|tho|bon|esh|oun|il|ili|ert|pon|nin|kistan|uela|xique|siege|di|ji|men|nam|ogo|ad|ade|embourg|mark|al|mala|kong|ice|pour|ay|ka|bwe)$/i', $country, $matches))
-			$suffix_preposition .= $array_preposition[2];
+	if  (preg_match('/(agne|iche|ran|ance|lande|nde|èce|ogne|aine|eine|ène|rse|la|ence|erre|dor|ie|ak|ine|é|ée|rica|oupe|oire|iche|ïdjan|nistan|sud|ège|ède|ypte|ique|teur|isse)$/i', $country, $matches) )
+		$prep_country = 'en '.ucfirst($country);
+	else if (preg_match('/(ar|ram|eille|nade|ris|ba|na|ma|co|ago|alu|boul|bul|eïn|ain|nei|ti|ok|pre|orre|lize|ypre|grenadine|ey)$/i', $country, $matches))
+		$prep_country = 'à '.ucfirst($country);
+	else if (preg_match('/(aso|am|ban|oc|os|ana|odge|bas|tho|bon|esh|oun|il|ili|ert|pon|nin|kistan|uela|xique|siege|di|ji|men|nam|ogo|ad|ade|rou|bourg|mark|al|mala|kong|ice|pour|ay|ka|bwe|uni)$/i', $country, $matches))
+		$prep_country = 'au '.ucfirst($country);
 	else if (preg_match('/(Îles|unis|elles|ores|ans|mas|ives|udes)$/i', $country, $matches))
-			$suffix_preposition .= $array_preposition[3];
-
-	return $suffix_preposition.' '.ucfirst($country);
+ 		$prep_country = 'aux '.ucfirst($country);
+	return $prep_country;
 }
 
 
 /* Test array */
 $countries = array (
+	'Madagascar',
+	'Sénégal',
+	'Cameroun',
 	'Allemagne',
+	'Maroc',
 	'Andorre',
 	'Autriche',
 	'Belgique',
@@ -39,6 +41,12 @@ $countries = array (
 	'Cambodge',
 	'Irlande',
 	'Italie',
+	'Algérie',
+	'Comores',
+	'Pakistan',
+	'Turquie',
+	'Liban',
+	'Iran',
 	'Luxembourg',
 	'Monaco',
 	'Norvège',
@@ -47,9 +55,17 @@ $countries = array (
 	'Portugal',
 	'Royaume-Uni',
 	'Slovaquie',
-	'Brésil'
+	'Brésil',
+	'Fidji',
+	'Nouvelle-Zélande',
+	'Australie',
+	'Equateur',
+	'Chili',
+	'Pérou',
+	'Djibouti',
+	'Inde'
 );
 
 /* Execute */
-foreach ($countries as $item) echo getPreposition_FR ($item) . '<br/>';
+foreach ($countries as $item) echo getPreposition_FR ($item) . "\n";
 
