0e4fd468f47c02a17898c98d8298aa3e90b8d54a
Sahad M <sahad.mohamed@gmail.com>
2015-08-16 03:01:46 +0200

    Clear syntax and fix words

--- test.php
+++ test.php
@@ -10,60 +10,99 @@
 
 function getPreposition_FR ($country) 
 {
-	$prep_country = '';
+    $prep_country = '';
+    
+    // EN
+    if  (preg_match('/(agne|iche|ran|ance|lande|nde|èce|ogne|aine|eine|ène|rse|la|ence|erre|dor|ie|ak|ine|é|ée|oupe|oire|iche|ïdjan|nistan|sud|ège|ède|ypte|bique|gique|nique|tique|teur|isse|ion)$/i', $country, $matches) )
+    {
+       $prep_country = 'en '.$country;
+    }
+    
+    // A
+    else if (preg_match('/(ar|ram|eille|nade|ris|ba|na|ma|co|ago|ong|alu|boul|bul|eïn|ain|nei|ti|ok|pre|pour|orre|lize|ypre|grenadine|ey)$/i', $country, $matches))
+    {
+       $prep_country = 'à '.$country;
+    }
+    
+    // AU
+    else if (preg_match('/(aso|am|ban|oc|os|ana|odge|bas|tho|bon|esh|oun|ica|il|ili|ert|pon|nin|kistan|uela|exique|siege|di|ji|men|nam|ogo|ad|ade|rou|bourg|mark|al|mala|kong|ice|pour|ay|ka|bwe|uni|nuatu|stein)$/i', $country, $matches))
+    {
+       $prep_country = 'au '.$country;
+    }
 
-	if  (preg_match('/(agne|iche|ran|ance|lande|nde|èce|ogne|aine|eine|ène|rse|la|ence|erre|dor|ie|ak|ine|é|ée|rica|oupe|oire|iche|ïdjan|nistan|sud|ège|ède|ypte|ique|teur|isse)$/i', $country, $matches) )
-		$prep_country = 'en '.ucfirst($country);
-	else if (preg_match('/(ar|ram|eille|nade|ris|ba|na|ma|co|ago|alu|boul|bul|eïn|ain|nei|ti|ok|pre|orre|lize|ypre|grenadine|ey)$/i', $country, $matches))
-		$prep_country = 'à '.ucfirst($country);
-	else if (preg_match('/(aso|am|ban|oc|os|ana|odge|bas|tho|bon|esh|oun|il|ili|ert|pon|nin|kistan|uela|xique|siege|di|ji|men|nam|ogo|ad|ade|rou|bourg|mark|al|mala|kong|ice|pour|ay|ka|bwe|uni)$/i', $country, $matches))
-		$prep_country = 'au '.ucfirst($country);
-	else if (preg_match('/(Îles|unis|elles|ores|ans|mas|ives|udes)$/i', $country, $matches))
- 		$prep_country = 'aux '.ucfirst($country);
-	return $prep_country;
+    // AUX
+    else if (preg_match('/(Îles|unis|elles|ores|ans|mas|ives|udes)$/i', $country, $matches))
+    { 
+       $prep_country = 'aux '.$country;
+    }
+    
+    return $prep_country;
 }
 
 
 /* Test array */
 $countries = array (
-	'Madagascar',
-	'Sénégal',
-	'Cameroun',
-	'Allemagne',
-	'Maroc',
-	'Andorre',
-	'Autriche',
-	'Belgique',
-	'Danemark',
-	'Espagne',
-	'Finlande',
-	'France',
-	'Cambodge',
-	'Irlande',
-	'Italie',
-	'Algérie',
-	'Comores',
-	'Pakistan',
-	'Turquie',
-	'Liban',
-	'Iran',
-	'Luxembourg',
-	'Monaco',
-	'Norvège',
-	'Pays-bas',
-	'Pologne',
-	'Portugal',
-	'Royaume-Uni',
-	'Slovaquie',
-	'Brésil',
-	'Fidji',
-	'Nouvelle-Zélande',
-	'Australie',
-	'Equateur',
-	'Chili',
-	'Pérou',
-	'Djibouti',
-	'Inde'
+    'Madagascar',
+    'Sénégal',
+    'Cameroun',
+    'Allemagne',
+    'Maroc',
+    'Andorre',
+    'Autriche',
+    'Belgique',
+    'Danemark',
+    'Espagne',
+    'Finlande',
+    'France',
+    'Cambodge',
+    'Irlande',
+    'Italie',
+    'Algérie',
+    'Comores',
+    'Pakistan',
+    'Turquie',
+    'Liban',
+    'Iran',
+    'Luxembourg',
+    'Monaco',
+    'Norvège',
+    'Pays-bas',
+    'Pologne',
+    'Portugal',
+    'Royaume-Uni',
+    'Slovaquie',
+    'Brésil',
+    'Fidji',
+    'Nouvelle-Zélande',
+    'Australie',
+    'Equateur',
+    'Chili',
+    'Pérou',
+    'Djibouti',
+    'Inde',
+    'Russie',
+    'Japon',
+    'Thailande',
+    'Sri-Lanka',
+    'Lesotho',
+    'Colombie',
+    'Grèce',
+    'Corée du Sud',
+    'Suisse',
+    'Réunion',
+    'Seychelles',
+    'Mozambique',
+    'Lienstenstein',
+    'Mexique',
+    'Vanuatu',
+    'Nouvelle Calédonie',
+    'Nouvelle Zélande',
+    'Antartique',
+    'Islande', 
+    'Panama',
+    'Cuba',
+    'Costa Rica',
+    'Mongolie'
 );
 
 /* Execute */
